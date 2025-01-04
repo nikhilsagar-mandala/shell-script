311c18070b3ef1e63bbad0180e3bc2fb7be85b69
nikhilsagar-mandala <nikhilsagarmandala@gmail.com>
2025-01-04 12:39:28 +0530

    Variables1

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+
+echo "Dhoni :: Hi Virat"
+echo "Virat :: Yes Dhoni, tell me"
+echo "Dhoni :: How're you doing Virat?"
+echo "Virat :: I am fine Dhoni, how about you?"
+echo "Dhoni :: I am good"
