699bfd3ae8343584072792f2b9bb64e002b90fe2
nikhilsagar-mandala <nikhilsagarmandala@gmail.com>
2025-01-05 00:53:35 +0530

    practice

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-
-# =, ne, gt, lt, ge, le
+#Program1
 NUMBER=$1
-if[$NUMBER -ge 100]
+if [$NUMBER -ge 100]
 then
-    echo "Number is greater than or equal to 100"
+echo "Number is greater than or equal to 100"
 else
-    echo "Number is less than 100"
+echo "Number is less than 100"
+fi
+
+#Program2
+DAY=$1
+if [$DAY -eq Sunday ]
+then
+echo "Today is Holiday because today is $DAY"
+else
+echo "Today is Working Day because today is $DAY"
 fi
