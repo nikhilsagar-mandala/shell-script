fdd2788e8f4bb14c07b4007856e37b0fe73a02ff
nikhilsagar-mandala <nikhilsagarmandala@gmail.com>
2025-01-04 12:32:08 +0530

    Hello World Program

--- helloworld.sh
+++ helloworld.sh
@@ -3,3 +3,6 @@
 # This for remininding that except in first line for shebang, wherever we use # it's for commenting
 
 echo "Hello World!"
+echo "Hi, World!"
+
+echo "This is my very first program in shell scripting"
