ce89a48da104f0beb63b451020c9a45ee37284db
nikhilsagar-mandala <nikhilsagarmandala@gmail.com>
2025-01-04 13:25:01 +0530

    05-variables.sh

---
+++ 05-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Please enter your username :: "
+
+read -s USERNAME
+# read -s is used for hiding the inputs which we're giving to the variables
+echo "User name is : ${USERNAME}
+echo "Please give your password :: "
+
+read -s PASSWORD
+
+
