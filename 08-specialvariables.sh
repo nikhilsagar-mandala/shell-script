74eae976fb5946c8e567a05db8e4554114d43f73
nikhilsagar-mandala <nikhilsagarmandala@gmail.com>
2025-01-05 00:05:43 +0530

    practice

--- 08-specialvariables.sh
+++ 08-specialvariables.sh
@@ -5,6 +5,7 @@ echo "All Variables passed is : $@"
 echo "Number of variables passed is : $#"
 echo "Script name is : $0"
 echo " Procees ID is : $$"
+sleep 100&
 echo "Process ID in background is : $!"
 echo "Present Working directory is : $PWD"
 echo "User is : $USER"
