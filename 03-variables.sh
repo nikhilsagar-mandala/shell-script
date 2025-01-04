b799c2d1ffb97140f274e907ba79d8a19c53f918
nikhilsagar-mandala <nikhilsagarmandala@gmail.com>
2025-01-04 13:03:10 +0530

    02-variables.sh

---
+++ 03-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# We denote the variables in Shell Scripting with "CAPITAL LETTERS"
+# For exampe "PERSON1"
+
+PERSON1=Suresh
+PERSON2=Ramesh  # Here for equal we should not give space
+
+
+
+echo "$PERSON1 :: Hi $PERSON2"     # To access the variable we need to use dollar i.e $
+echo "$PERSON2 :: Yes $PERSON1, tell me"
+echo "$PERSON1 :: How're you doing $PERSON1?"
+echo "$PERSON2 :: I am fine $PERSON1, how about you?"
+echo "$PERSON1 :: I am good"
