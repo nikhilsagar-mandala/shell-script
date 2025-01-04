dcb6fec3062009a690b8559c4a3a7eb4a85df1d7
nikhilsagar-mandala <nikhilsagarmandala@gmail.com>
2025-01-04 23:18:44 +0530

    practice

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,6 +1,6 @@
 MOVIES=("Pushpa" "rrr" "kgf")
 
-echo "First movie is : $MOVIES[0]"
-echo "Second movie is : $MOVIES[1]"
-echo "Third movie is : $MOVIES[2]"
-echo "All movies = $MOVIES[@]"
+echo "First movie is : ${MOVIES[0]}"
+echo "Second movie is : ${MOVIES[1]}"
+echo "Third movie is : ${MOVIES[2]}"
+echo "All movies = ${MOVIES[@]}"
