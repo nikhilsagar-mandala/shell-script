dc4bd85bb245a4509b6028e0eaae4012c6a2cd69
nikhilsagar-mandala <nikhilsagarmandala@gmail.com>
2025-01-05 20:42:57 +0530

    practice

--- 10-installscript.sh
+++ 10-installscript.sh
@@ -21,3 +21,19 @@ echo " mysql is not installed "
 else
 echo " mysql is installed successfully "
 fi
+
+dnf list insatlled git -y
+if [ $? -ne 0 ]
+then
+dnf install git -y
+else 
+echo " git is already installed "
+fi
+
+dnf list installed git -y
+if [ $? -ne 0 ]
+then 
+echo " git is not installed "
+else
+echo " git is installed successfully "
+fi
